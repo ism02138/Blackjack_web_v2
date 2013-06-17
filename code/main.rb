76a8c6550ac0d884fa27cd8baf85702c5f27f8f6
Ian Moulton <ian.moulton@gmail.com>
2013-06-17 18:45:22 -0400

    parts 1 & 2 complete, much code consultation required

--- code/main.rb
+++ code/main.rb
@@ -4,20 +4,36 @@ require 'pry'
 
 set :sessions, true
 
-
-get '/' do				# router action
-	"Hello World!!"	# controller
+get '/' do			
+	session[:computer_name] = "Evil Computer"
+	if session[:player_name]
+		redirect '/play_game'
+	else
+		redirect '/new_player'
+	end
 end
 
-get '/hello' do
-	@name = 'ian'
-	erb :test
+get '/new_player' do 
+	erb :new_player
 end
 
-get '/redirect' do
-	redirect '/hello'
+post '/new_player' do 
+	session[:player_name] = params[:player_name]
+	redirect '/play_game'
 end
 
-post '/my_form' do
-	puts params['username']
+get '/play_game' do
+	suits = ['H', 'D', 'S', 'C']
+	values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+	session[:deck] = suits.product(values).shuffle!
+
+	session[:player_hand] = []
+	session[:computer_hand] = []
+
+	session[:player_hand] << session[:deck].pop
+	session[:computer_hand] << session[:deck].pop
+	session[:player_hand] << session[:deck].pop
+	session[:computer_hand] << session[:deck].pop
+
+	erb :play_game
 end
