e4082755a9ca326b783bd81101ab88e00afa9176
Ian Moulton <ian.moulton@gmail.com>
2013-06-18 19:19:22 -0400

    hit/stay buttons & card display

--- code/main.rb
+++ code/main.rb
@@ -23,8 +23,8 @@ post '/new_player' do
 end
 
 get '/play_game' do
-	suits = ['H', 'D', 'S', 'C']
-	values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+	suits = ['hearts', 'diamonds', 'clubs', 'spades']
+	values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace']
 	session[:deck] = suits.product(values).shuffle!
 
 	session[:player_hand] = []
@@ -35,5 +35,29 @@ get '/play_game' do
 	session[:player_hand] << session[:deck].pop
 	session[:computer_hand] << session[:deck].pop
 
+	session[:player_total] = hand_sum(session[:player_hand])
+	session[:computer_total] = hand_sum(session[:computer_hand])
+
 	erb :play_game
+end
+
+def hand_sum(hand)
+	sum = [0, 0]
+	hand.each { |suit, value|
+		if value == 'jack' || value == 'queen' || value == 'king'
+			sum[0] += 10
+			sum[1] += 10
+		elsif value == 'ace'
+			sum[0] += 1
+			sum[1] += 11
+		else
+			sum[0] += value.to_i
+			sum[1] += value.to_i
+		end
+	}
+	if sum[1] > 21 || sum[0] == sum[1]
+		sum.pop
+		return "#{sum[0]}"
+	end	
+	return "#{sum[0]} or #{sum[1]}"
 end
